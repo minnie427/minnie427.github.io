4f395ceba2c15badb7b96dfa7be6941a1661a00a
minnie427 <110432724+minnie427@users.noreply.github.com>
2022-09-06 12:23:45 +1000

    Add files via upload

---
+++ exercise3/filter.js
@@ -0,0 +1,21 @@
+function filterCategory(filterSelection, filterBy, filterButton){
+    let recipeCards = Array.from(document.getElementsByClassName ("RecipeCardWrapper"));
+    //console.log(recipeCards);
+
+    for(let listItem of recipeCards){
+    //console.log(listem. datest. author)
+        if(listItem.dataset[filterBy] === filterSelection){
+        listItem.style.display = "flex";
+        } else {
+        listItem.style.display = "none";
+        }
+    }
+}
+
+function allCategories(){
+    let recipeCards = Array.from(document.getElementsByClassName ("RecipeCardWrapper"));
+    //console.log(recipeCards);
+    for(let listItem of recipeCards){
+        listItem.style.display = "flex";
+    }
+}
