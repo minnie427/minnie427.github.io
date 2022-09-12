7fa51410d63f2d39d52fa65d523c6472c9bb1c28
minnie427 <110432724+minnie427@users.noreply.github.com>
2022-09-12 23:05:16 +1000

    Add files via upload

--- exercise3/filter.js
+++ exercise3/filter.js
@@ -1,10 +1,14 @@
-/* This is the filter function for the food categories */
+/* This is the filter function for the food categories except "all" */
 function filterCategory(filterSelection, filterBy, filterButton){
+    /* Array is like a database storing list of information and it is an efficient way to organise all the listings under one object */
     let recipeCards = Array.from(document.getElementsByClassName ("RecipeCardWrapper"));
     //console.log(recipeCards);
 
+    /* This is to filter and show only what's selected within categories */
     for(let listItem of recipeCards){
     //console.log(listem. datest. author)
+
+    /* if it's the one selected, then it shows the item in "flex", if not, it will not show the item */
         if(listItem.dataset[filterBy] === filterSelection){
         listItem.style.display = "flex";
         } else {
@@ -12,7 +16,7 @@ function filterCategory(filterSelection, filterBy, filterButton){
         }
     }
 }
-
+/* This is for the "all" in each of 3 categories */
 function allCategories(){
     let recipeCards = Array.from(document.getElementsByClassName ("RecipeCardWrapper"));
     //console.log(recipeCards);
